05397043e3f63695e2fe9f760b5ea4b353160057
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-21 14:14:57 +0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,51 @@
 "use strict";
 
-const isFieldEmpty = (field) => {
-   return (field === "") | (field === "0") ? true : false;
+/* check if field is empty */
+const isFieldEmpty = (fields) => {
+   let empty = true;
+   for (let i = 0; i < fields.length; i++) {
+      if (fields[i].value !== "" || fields[i] === "0") {
+         empty = false;
+      }
+   }
+   return empty;
 };
 
-const fields = document.querySelectorAll(".field");
-const reset = document.querySelector(".reset .btn");
+/* if field is empty disable reset button */
+const resetBtnActive = (fields) => {
+   const resetBtn = document.querySelector(".reset .btn");
+
+   if (!isFieldEmpty(fields)) {
+      resetBtn.classList.remove("active");
+   } else {
+      resetBtn.classList.add("active");
+   }
+};
+
+/* clear all field */
+const reset = (fields, value) => {
+   if (value === "reset") {
+      fields.forEach((field) => {
+         field.value = "";
+         resetBtnActive(0);
+      });
+   }
+};
+
+/* waiting for changes in input field */
+const fields = document.querySelectorAll(".field input");
 
 fields.forEach((field, key) => {
-   field.addEventListener("input", (e) => {
-      e.target.value = e.target.value.toLocaleString("en-US");
-      console.log("this", e.target.value);
-
-      //   if (!isFieldEmpty(value)) {
-      //      reset.classList.remove("active");
-      //   } else {
-      //      reset.classList.add("active");
-      //   }
+   field.addEventListener("input", (event) => {
+      resetBtnActive(fields);
    });
 });
 
+/* waiting for click */
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach((btn, key) => {
    btn.addEventListener("click", (event) => {
-      console.log("this is test click", btn, event.target.value);
+      reset(fields, event.target.value);
    });
 });
